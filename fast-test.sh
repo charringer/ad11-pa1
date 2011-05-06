cb746a19c53ce62b4f6367498374e3bd56dd6519
Claudio Harringer <der.claudio@gmail.com>
2011-05-06 14:37:55 +0200

    Schnelltestskript

---
+++ fast-test.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# Test the code quickly without using the clumsy ant script.
+
+javac src/Sorter.java -classpath build -d build/ads1ss11/pa1/ && java -cp build ads1ss11.pa1.Main < testdata/0000
