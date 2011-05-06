79a8e4fa74f53cd70ae4228c74fd4990c0ba78a1
Claudio Harringer <der.claudio@gmail.com>
2011-05-06 17:30:17 +0200

    Verwende zyklische statt azyklische Liste

--- src/Sorter.java
+++ src/Sorter.java
@@ -25,7 +25,7 @@ public class Sorter {
 	 * @return Sortierte Eingabefolge
 	 */
 	public DoublyLinkedList mergesort(DoublyLinkedList in, int numOfElements) {
-		ListElement first = acyclicSinglyLinked(in);
+		ListElement first = cyclicSinglyLinked(in);
 
 		first = mergesort_intern(first, numOfElements);
 
@@ -33,13 +33,13 @@ public class Sorter {
 	}
 
 	/**
-	 * Wandelt in eine azyklische einfach verkettete Liste um.
+	 * Wandelt in eine zyklische einfach verkettete Liste um.
 	 *
 	 * @param in zyklische doppelt verkettete Liste
 	 * @return erstes Element der Liste
 	 */
-	protected ListElement acyclicSinglyLinked(DoublyLinkedList in) {
-		//System.out.println("make acyclicSinglyLinked");
+	protected ListElement cyclicSinglyLinked(DoublyLinkedList in) {
+		//System.out.println("make cyclicSinglyLinked");
 
 		ListElement current = in.first;
 		while (current.next != in.first) {
@@ -49,7 +49,7 @@ public class Sorter {
 		}
 		//System.out.println("original "+current);
 		current.prev = null;
-		current.next = null;
+		current.next = in.first;
 		return in.first;
 	}
 
@@ -57,14 +57,14 @@ public class Sorter {
 	 * Wandelt in eine zyklische doppelt verkettete Liste um.
 	 *
 	 * @param list Listen-Datenstruktur, in die geschrieben werden soll
-	 * @param first erstes Element der azyklischen einfach verketteten Liste
+	 * @param first erstes Element der zyklischen einfach verketteten Liste
 	 * @return list
 	 */
 	protected DoublyLinkedList cyclicDoublyLinked(DoublyLinkedList list, ListElement first) {
 		//System.out.println("make cyclicDoublyLinked");
 
 		ListElement current = first;
-		while (current.next != null) {
+		while (current.next != first) {
 			//System.out.println("final "+current);
 			current.next.prev = current;
 			current = current.next;
@@ -80,7 +80,7 @@ public class Sorter {
 	/**
 	 * Mergesort.
 	 *
-	 * @param list azyklische einfach verkettete Liste
+	 * @param list zyklische einfach verkettete Liste
 	 * @param numOfElements Anzahl der Elemente
 	 * @return erstes Element der sortierten Liste
 	 */
@@ -104,55 +104,63 @@ public class Sorter {
 	}
 
 	/**
-	 * Teile (azyklische einfach verkettete Liste) in zwei Listen.
+	 * Teile (zyklische einfach verkettete Liste) in zwei Listen.
 	 *
-	 * @param list erstes Element der ursprünglichen Liste
+	 * @param first erstes Element der ursprünglichen Liste
 	 * @param skip &ge; 1
 	 * @return erstes Element des abgespaltenen hinteren Teils der Liste
 	 */
-	protected ListElement split(ListElement list, int skip) {
-		ListElement endOfFirst = list;
+	protected ListElement split(ListElement first, int skip) {
+		ListElement endOfFirst = first;
 		for (int i = 0; i < skip-1; i++) {
 			endOfFirst = endOfFirst.next;
 		}
+		ListElement endOfSecond = endOfFirst.next;
+		while (endOfSecond.next != first) {
+			endOfSecond = endOfSecond.next;
+		}
 		ListElement second = endOfFirst.next;
-		endOfFirst.next = null;
+		endOfFirst.next = first;
+		endOfSecond.next = second;
 
 		return second;
 	}
 
 	/**
-	 * Führt zwei sortierte (azyklische einfach verkettete) Listen zusammen.
+	 * Führt zwei sortierte (zyklische einfach verkettete) Listen zusammen.
 	 *
-	 * @param a erste Liste, &ne; null
-	 * @param b zweite Liste, &ne; null
+	 * @param firsta erste Liste, &ne; null
+	 * @param firstb zweite Liste, &ne; null
 	 * @return erstes Element der zusammengeführten Liste
 	 */
-	protected ListElement merge(ListElement a, ListElement b) {
-		//System.out.println("merge("+list2string(a)+","+list2string(b)+")");
+	protected ListElement merge(ListElement firsta, ListElement firstb) {
+		//System.out.println("merge("+list2string(firsta)+","+list2string(firstb)+")");
 
+		ListElement a = firsta;
+		ListElement b = firstb;
 		ListElement current = null;
 		if (a.getKey() <= b.getKey()) {
 			current = a;
-			a = a.next;
+			a = (a.next==firsta) ? null : a.next;
 		} else {
 			current = b;
-			b = b.next;
+			b = (b.next==firstb) ? null : b.next;
 		}
 		//System.out.println("merge "+current);
 		ListElement first = current;
 
 		while (a != null || b != null) {
-			if (b == null || (a!= null && a.getKey() <= b.getKey())) {
+			if (b == null || (a != null && a.getKey() <= b.getKey())) {
 				current.next = a;
-				a = a.next;
+				a = (a.next==firsta) ? null : a.next;
 			} else {
 				current.next = b;
-				b = b.next;
+				b = (b.next==firstb) ? null : b.next;
 			}
 			current = current.next;
 			//System.out.println("merge "+current);
 		}
+		current.next = first;
 
 		//System.out.println("result of merge: "+list2string(first));
 		return first;
@@ -161,7 +169,7 @@ public class Sorter {
 	/**
 	 * Liste anzeigen.
 	 *
-	 * @param first erstes Element einer zyklischen oder azyklischen Liste, &ne; null
+	 * @param first erstes Element einer zyklischen oder zyklischen Liste, &ne; null
 	 */
 	protected String list2string(ListElement first) {
 		if (first == null) {
